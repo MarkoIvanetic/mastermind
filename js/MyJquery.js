9621c6f244c154a1dc84e659fa02d06089c5d382
MarkoIvanetic <manna.grad@gmail.com>
2015-04-29 23:00:13 +0200

    Adding colors to colorContainer via colorButtons implemented

--- js/MyJquery.js
+++ js/MyJquery.js
@@ -1,13 +1,15 @@
   $(document).ready(function () {
-  	var red = {'background-color': 'rgb(255,0,0)'};
-	var blue = {'background-color': '#0000FF'};
-	var yellow = {'background-color': '#FFFF00'};
-	var green = {'background-color': '#008000'};
-	var purple = {'background-color': '#800080'};
-	var orange = {'background-color': '#FF4500'};
+  	var red = {'background-color': 'rgb(255, 0, 0)'};
+	var blue = {'background-color': 'rgb(0, 0, 255)'};
+	var yellow = {'background-color': 'rgb(255, 255, 0)'};
+	var green = {'background-color': 'rgb(0, 128, 0)'};
+	var purple = {'background-color': 'rgb(128, 0, 128)'};
+	var orange = {'background-color': 'rgb(255, 69, 0)'};
 	/*var lightBlue = {'background-color': '#5D9BC7'};*/
 
 	var i=0;
+	var colorCounter = 1;
+	var rowCounter = 1;
 	/*Assign colors to colorButtons*/
 	var colors = ["red","blue","yellow","green","purple","orange"];
 	$('.colorButton').each(function(){
@@ -23,11 +25,21 @@
 	alert(solution);
 	//check colorButton
 	$('.colorButton').click(function(){
-		alert($(this).css("background-color"));
-		$.each(red, function(key, value) {
-    	alert(this, red[value]);
-		});
-	})
+		//alert($(this).css("background-color"));
+		//Object literal pattern
+		/*	
+		$.each(eval(colors[i]), function(key, value) {
+    	alert(this, eval(colors[i])[value]);
+		});*/
+		$('.colorSection:nth-child('+ rowCounter +') > .colors:nth-child(' + colorCounter + ')').css("background-color",$(this).css("background-color"));
+		colorCounter++;
+	});//End color button click
+	$('#submit').click(function(){
+		//iterate ColorSections and reset colors
+		rowCounter++;
+		colorCounter=0;
+		//alert(rowCounter);
+	});
 
 
 
