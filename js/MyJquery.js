af5f10aeb6a5c1e62466b432abd3b5236bcc276f
MarkoIvanetic <manna.grad@gmail.com>
2015-04-29 15:41:28 +0200

    Added colors, color assignation, onclick return color of a button

--- js/MyJquery.js
+++ js/MyJquery.js
@@ -1,3 +1,34 @@
   $(document).ready(function () {
+  	var red = {'background-color': 'rgb(255,0,0)'};
+	var blue = {'background-color': '#0000FF'};
+	var yellow = {'background-color': '#FFFF00'};
+	var green = {'background-color': '#008000'};
+	var purple = {'background-color': '#800080'};
+	var orange = {'background-color': '#FF4500'};
+	/*var lightBlue = {'background-color': '#5D9BC7'};*/
+
+	var i=0;
+	/*Assign colors to colorButtons*/
+	var colors = ["red","blue","yellow","green","purple","orange"];
+	$('.colorButton').each(function(){
+		$(this).css(eval(colors[i]));
+		i++;
+	})/*End Assign colors to colorButtons*/
+	/*Define a solution, random color push to solution array*/
+	var solution = [];
+	for(i = 0; i < 4; i++) {
+     solution.push(colors[Math.floor(Math.random()*colors.length)]);
+	}
+	//Check solution
+	alert(solution);
+	//check colorButton
+	$('.colorButton').click(function(){
+		alert($(this).css("background-color"));
+		$.each(red, function(key, value) {
+    	alert(this, red[value]);
+		});
+	})
+
+
 
 });//End document.ready
